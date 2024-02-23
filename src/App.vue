7b885c7d300fc2e104a47d48412c1b083d432471
unknown <chirayair@gmail.com>
2024-02-23 16:41:28 -0400

    initial project

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import MenuDest from "./components/MenuDest.vue"
+import { ref } from "vue";
+import ModalDest from "./components/ModalDest.vue";
+
+const show = ref(false)
+
+</script>
+
+<template>
+  <button @click="show = !show">Menu</button>
+  <Transition name="fade">
+    <MenuDest v-show="show"/>
+  </Transition>
+  <div>
+    <ModalDest/>
+  </div>
+</template>
+
+<style scoped>
+.fade-leave-to {
+  opacity: 0;
+}
+
+.fade-leave-active,
+.fade-from-active {
+  transition: opacity 0.5s ease;;
+}
+</style>
---
+++ src/components/MenuDest.vue
@@ -0,0 +1,7 @@
+<template>
+    <ul>
+        <li>opt1</li>
+        <li>opt2</li>
+        <li>opt3</li>
+    </ul>
+</template>
