328ad49de70cdd387ba8d5298d64fe714d2b2078
unknown <chirayair@gmail.com>
2024-02-23 17:08:34 -0400

    push to class-10

--- src/components/ModalDest.vue
+++ src/components/ModalDest.vue
@@ -16,11 +16,7 @@ const toggle = () => {
             Modal
         </button>
         <Teleport to="body">
-            <div v-show="show" class="modal">
-                <h1>Titulo</h1>
-                <p>who is this person? her name is maria </p>
-                <button @click="toggle">Close</button>
-            </div>
+
         </Teleport>
     </div>
 </template>
