aae5f08bfdac8cc3267ccea8f6f849aaf3a8a175
unknown <chirayair@gmail.com>
2024-03-01 18:53:36 -0400

    script setup

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <script setup>
+import { ref, provide } from "vue";
 import HomeApp from "./components/HomeApp.vue"
+
+const name = ref('otr')
+
+provide("userName", "yairNeeeerd")
 
 </script>
 
 <template>
   <div>
-    <HomeApp></HomeApp>
+    <HomeApp :var1="name" var2="Martinez"></HomeApp>
   </div>
 </template>
--- src/components/HomeApp.vue
+++ src/components/HomeApp.vue
@@ -1,14 +1,62 @@
 <script setup>
-import { onMounted } from 'vue';
+import { ref, computed, defineProps, inject, watch} from 'vue';
 
-onMounted(() =>{
-    console.log('the component is mounted')
+const user = inject("userName",null)
+
+const props = defineProps({
+    var1: String,
+    var2: String,
 })
 
+
+const firstName = ref('Yair');
+const lastName = ref('Chira');
+
+const otherName = `${props.var1} ${props.var2}`
+
+const fullName = computed(() => {
+    return `${firstName.value} ${lastName.value }`
+});
+
+const btn = ref(null);
+watch(btn, (valor) => {
+
+    console.log(valor);
+})
+
+//        USO DE REF()
+// const dataCounter = ref({
+//     counter: 0,
+// });
+//        USO DE REACTIVE()
+// const dataCounterReactive = reactive({
+//     counter: 0,
+// });
+
+
+//setInterval(() => dataCounter.value.counter++, 5000);
+//setInterval(() => dataCounterReactive.counter++, 5000);
+
+
+//        USO DE WATCH
+// watch(() => dataCounterReactive.counter,
+//         ( valor, anterior) => {
+//             console.log(valor, anterior)
+//         })
+// Con el uso de ref
+//   const count = ref(0)
+//   watch(count, (count, prevCount) => {
+//      console.log(count, prevCount)
+// })
 </script>
+
 
 <template>
     <div>
-        Hello
+        {{ fullName }} {{ otherName }} {{ user }}
     </div>
+    <button ref="btn">Click</button>
+
+
+    <!-- <p>{{ dataCounterReactive.counter }}</p> -->
 </template>
